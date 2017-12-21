59280931d2de2c8c27566354e02c558162e8efa7
devsadventure <margaret.the.adventurer@gmail.com>
2017-12-21 17:04:35 +0900

    로직 완성

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ function randomColor(){
   return `rgb(${random255()}, ${random255()}, ${random255()})`;
 }
 
-document.querySelectorAll('.box').forEach(el => {
-  el.style.backgroundColor = randomColor();
-})
+let stage;
+let problemSet;
+let correctAnswer;
+
+function init(){
+  stage = 0;
+  problemSet = [randomColor(), randomColor(), randomColor()];
+  correctAnswer = Math.floor(Math.random() * 3);
+}
+
+function nextStage(){
+  stage++;
+  problemSet = [randomColor(), randomColor(), randomColor()];
+  correctAnswer = Math.floor(Math.random() * 3);
+}
+
+function draw(){
+  document.querySelectorAll('.box').forEach((el, index) => {
+    el.style.background = problemSet[index];
+  })
+  document.querySelector('.rgb-text').textContent = problemSet[correctAnswer];
+  document.querySelector('.score').textContent = stage;
+}
+
+document.querySelectorAll('.box').forEach((el, index) => {
+  el.addEventListener('click', e => {
+    if(index === correctAnswer){
+      nextStage();
+      draw();
+    } else {
+      init();
+      draw();
+    }
+  })
+});
+
+init();
+draw();
